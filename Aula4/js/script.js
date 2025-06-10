88d29abeae30a9d9a85fc54662646a938198d21b
Matheus-Nobre <matheusipsnobre@gmail.com>
2025-06-09 21:00:13 -0300

    Até a aula 5

---
+++ Aula4/js/script.js
@@ -0,0 +1,4 @@
+console.log("Primeiro");
+
+console.log("Segundo");
+
---
+++ Aula6/js/script.js
@@ -0,0 +1,13 @@
+var nome = "João";
+
+
+console.log(nome);
+
+let testando = 1;
+
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+ 
